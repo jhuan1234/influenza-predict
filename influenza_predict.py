48da479b93dfa6fc831a273a8fc33ae513c8c0a7
jhuan1234 <140406994+jhuan1234@users.noreply.github.com>
2024-09-03 11:23:12 +0800

    Update influenza_predict.py

--- influenza_predict.py
+++ influenza_predict.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 # Load the model
 model = joblib.load('rf_model.pkl')
-
+scale = joblib.load('scaler.pkl')
 # Define feature names
 feature_names = [
     "NEUT%", "PCT", "AST", "Glucose", "BUN", "C3", "B cell%", "CD4+ Tcell count"
@@ -75,7 +75,8 @@ if st.button("Predict"):
 
    # Calculate SHAP values and display force plot   
     explainer = shap.TreeExplainer(model)   
-    shap_values = explainer.shap_values(features)
+    features_scale=pd.DataFrame(scale.transform(features),columns=feature_names)
+    shap_values = explainer.shap_values(features_scale)
     print(shap_values,features.shape)
     shap.force_plot(explainer.expected_value[1], shap_values[1],features,feature_names=feature_names,show=False,matplotlib=True)
     plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=1200)
